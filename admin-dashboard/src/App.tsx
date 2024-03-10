015353e682905f8947d3285bef7b55c9043c60d5
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-03-10 18:39:32 +0530

    early routes established

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -1,11 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy } from "react";
 
+const Dashboard = lazy(() => import(("./pages/Dashboard")));
+const Products = lazy(() => import(("./pages/Products")));
+const Customers = lazy(() => import(("./pages/Customers")));
+const Transactions = lazy(() => import(("./pages/Transactions")));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: "/admin/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/admin/products",
+        element: <Products />
+      },
+      {
+        path: "/admin/customers",
+        element: <Customers />,
+      },
+      {
+        path: "/admin/transactions",
+        element: <Transactions />
+      }
+    ]
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
-      This is admin dashboard.
-    </div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
