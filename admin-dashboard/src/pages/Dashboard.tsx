210c6ba45fbaa307881b5d00eab488c1e077b7ca
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-04-26 22:34:14 +0530

    Revenue and Transaction: chart made with dynamic data

--- admin-dashboard/src/pages/Dashboard.tsx
+++ admin-dashboard/src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { FaRegBell } from "react-icons/fa";
 import userImg from "../assets/userpic.png";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 import data from "../assets/data.json";
+import { BarChart } from "../components/Charts";
 
 const Dashboard = () => {
   return (
@@ -49,6 +50,14 @@ const Dashboard = () => {
         <section className="graph-container">
           <div className="revenue-chart">
             <h2>Revenue & Transaction</h2>
+            <BarChart
+              data_1={[300, 144, 433, 655, 237, 755, 190]}
+              data_2={[200, 344, 556, 435, 347, 866,940]}
+              title_1="Revenue"
+              title_2="Transaction"
+              bgColor_1="rgb(0, 115, 255)"
+              bgColor_2="rgb(54, 162, 235, 0.8)"
+            />
           </div>
           <div className="dashboard-categories">
             <h2>Inventory</h2>
