5f9347f9a8ba53f806e77936556071946a85b04a
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-04-29 00:37:22 +0530

    Products table: interface datatype and array of column defined

--- admin-dashboard/src/pages/Products.tsx
+++ admin-dashboard/src/pages/Products.tsx
@@ -1,13 +1,53 @@
+import { ReactElement, useState } from "react";
 import AdminSidebar from "../components/AdminSidebar";
+import TableHOC from "../components/TableHOC";
+import { Column } from "react-table";
 
+interface DataType{
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  action: ReactElement;
+}
+
+const columns: Column<DataType>[] = [
+  {
+    Header: "Photo",
+    accessor: "photo",
+  },
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Price",
+    accessor: "price",
+  },
+  {
+    Header: "Stock",
+    accessor: "stock",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  },
+]
+
+const arr: DataType[] = [];
 
 const Products = () => {
+
+  const [data] = useState<DataType[]>(arr);
+
+  const Table = TableHOC<DataType>(
+    columns, data, "dashboard-product-box", "Products"
+  )
   return (
 <div className="admin-container">
       {/* Sidebar */}
       <AdminSidebar />
-      {/* Main content */}
-      <main>Main</main>
+      <main>{Table()}</main>
     </div>
   )
 }
