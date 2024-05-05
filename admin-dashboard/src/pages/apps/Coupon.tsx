5cf308f78e01fa4b75f09d48137c1a2c6c05164e
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-05-05 23:06:32 +0530

    coupon form and required states added

--- admin-dashboard/src/pages/apps/Coupon.tsx
+++ admin-dashboard/src/pages/apps/Coupon.tsx
@@ -1,10 +1,74 @@
+import { useState } from "react";
 import AdminSidebar from "../../components/AdminSidebar";
 
 const Coupon = () => {
+  const [size, setSize] = useState<number>(8);
+  const [prefix, setPrefix] = useState<string>("");
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeCharacters, setIncludeCharacters] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const [coupon, setCoupon] = useState<string>("");
+  const copyText = (coupon: string) => {  }
+
   return (
     <div className="admin-container">
       <AdminSidebar />
-      <main className="dashboard-app-container"></main>
+      <main className="dashboard-app-container">
+        <h1>Coupon</h1>
+        <section>
+          <form className="coupon-form">
+            <input
+              type="text"
+              placeholder="Text to include"
+              value={prefix}
+              onChange={(e) => setPrefix(e.target.value)}
+              maxLength={size}
+            />
+
+            <input
+              type="number"
+              placeholder="Coupon Length"
+              value={size}
+              onChange={(e) => setSize(Number(e.target.value))}
+              min={8}
+              max={25}
+            />
+
+            <fieldset>
+              <legend>Include</legend>
+              <input
+                type="checkbox"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers((prev) => !prev)}
+              />
+              <span>Numbers</span>
+              <input
+                type="checkbox"
+                checked={includeCharacters}
+                onChange={() => setIncludeCharacters((prev) => !prev)}
+              />
+              <span>Characters</span>
+              <input
+                type="checkbox"
+                checked={includeSymbols}
+                onChange={() => setIncludeSymbols((prev) => !prev)}
+              />
+              <span>Symbols</span>
+            </fieldset>
+            <button type="submit">Generate</button>
+          </form>
+          {coupon && (
+            <code>
+              {coupon}{" "}
+              <span onClick={() => copyText(coupon)}>
+                {isCopied ? "Copied" : "Copy"}
+              </span>
+            </code>
+          )}
+        </section>
+      </main>
     </div>
   );
 };
